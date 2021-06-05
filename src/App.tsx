44dc6a1bcbb3479f6f10123a97d5528b1f49100d
unknown <joaocordeiroalmeida01@gmail.com>
2021-06-05 18:50:41 -0300

    style page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
 import './styles/global.css'
+import './styles/pages/landing.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+          <img src={logo} alt="Logo Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong> Cianorte </strong>
+          <span> Paraná </span>
+        </div>
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+        </a>
+      </div>  
     </div>
   );
 }
