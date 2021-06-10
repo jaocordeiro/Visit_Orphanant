9e73fc65c0f16b279f06fd23b64c5b0e04874ab8
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2021-06-09 21:52:48 -0300

    component orphanages creating and routes apllication

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,5 @@
+export default function OrphanagesMap() {
+    return (
+        <h1>Estou aqi</h1>
+    )
+}
---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+			<Switch>
+            	<Route path="/" exact component={Landing} />
+				<Route path="/map" component={OrphanagesMap} />
+			</Switch>	
+        </BrowserRouter>
+    )
+}
