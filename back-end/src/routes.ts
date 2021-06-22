bc0833c4095aae2728c703efe64f39e718d34402
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2021-06-21 22:24:41 -0300

    structuring DB, insert images of orphanages

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadImg from './config/upload'
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadImg);
 
-routes.post('/orphanages', OrphanagesController.create);
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
     
-  export default routes;
+export default routes;
