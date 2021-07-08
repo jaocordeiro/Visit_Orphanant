0fdbdf9000bc02231b4ecd4649d6ec389a630554
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2021-07-08 20:39:16 -0300

    mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import mapMarker from './src/images/map-marker.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView 
+      provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -23.648388,
+          longitude: -52.6298881,
+          latitudeDelta: 0.009,
+          longitudeDelta: 0.009,
+        }}
+      >
+        <Marker 
+          icon={mapMarker}
+          coordinate={{
+            latitude: -23.648388,
+            longitude: -52.6298881,
+          }}
+        />
+      </MapView>  
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
+});
